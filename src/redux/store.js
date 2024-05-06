761ec9d68a7c49e9c68baedda1c20dd3fdfb5c94
hancom02 <20520484@gm.uit.edu.vn>
2024-05-06 22:35:14 +0700

    update handle Upload lesson

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import lessonReducer from './slices/lessonSlice'
-import authReducer from './slices/authSlice'
+import programReducer from './slices/programSlice'
 
 export const store = configureStore({
   reducer: {
     lesson: lessonReducer,
-    auth: authReducer,
+    program: programReducer,
+
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false
-  
+
       //{
       // Ignore these action types
       // ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
